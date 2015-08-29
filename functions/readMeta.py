b937df2a03503806eb5c49319cc8997bdf508f24
mbbderobles <mbbderobles@gmail.com>
2015-08-29 15:59:24 +0800

    Added some functions

---
+++ functions/readMeta.py
@@ -0,0 +1,43 @@
+#read metadata
+
+def readMetaData():
+	fReader = open("./data/metadata","r")
+	counter = 0
+	
+	tables = {} #list of tables from the metadata
+	tbl = '' #temp var for the name of the tables
+	counter += 1
+	for line in fReader:
+		if(line[-1]=='\n'):
+			line = line[:-1] #removes \n
+		cols = line.split(",")
+		if(len(cols)==1):	#creates a hash for a certain table
+			tables[cols[0]] = {}
+			tbl = cols[0];
+			counter = 0
+		else:			#creates a hash for each column of a table
+			tables[tbl][counter] = []
+			tables[tbl][counter].append(cols[0])
+			tables[tbl][counter].append(cols[1]) #type
+			tables[tbl][counter].append(int(cols[2])) #length
+			
+			if(len(cols) > 3):
+				tables[tbl][counter].append(True)
+			else:
+				tables[tbl][counter].append(False)
+			counter += 1
+
+	return tables
+
+#tb = readMetaData()
+'''
+#prints the table data
+for i in tb.keys():
+	print("\n",i.upper(),": ") #prints the table name
+	print("\tCOLUMN\t\tTYPE\t\tLENGTH\t\tISPK") #prints the headers
+	for j in tb[i].keys():	#prints each column details in a row
+		for k in tb[i][j]:
+			print("\t%s\t" %k,end=" ")
+		print()
+	print()
+'''
---
+++ functions/readTable.py
@@ -0,0 +1,42 @@
+import readMeta
+
+def getRowsFromTable(tblname,columns):
+	fReader = open('./data/'+tblname+'.csv','r')
+	tblData = {}
+	for line in fReader:
+		if(line[-1]=='\n'):
+			line = line[:-1]
+		data = line.split(',')
+		tblData[data[0]] = {}
+		i = 0
+		while i<len(columns):
+			if i==0:
+				i = i+1
+				continue
+			tblData[data[0]][columns[i]] = data[i]
+			i = i+1
+	return tblData
+
+def getColumns(tableName,tables):
+	cols = []
+	temp = tables[tableName]
+	for i in temp.keys():
+		cols.append(temp[i][0]) #sub zero since 1st element is columnName
+	return cols
+
+def printTableRows(rows,cols):
+	for k in cols:
+		print("\t%s\t" % k.upper(), end="")
+	print()
+	for k in rows.keys():
+		print("\t%s\t " % k, end="")
+		i=1
+		for j in rows[k]:
+			print("\t%s\t " % rows[k][cols[i]], end="")
+			i = i+1
+		print()
+		
+tb = readMeta.readMetaData()
+cols = getColumns("student",tb)
+rows = getRowsFromTable("student",cols)
+printTableRows(rows,cols)

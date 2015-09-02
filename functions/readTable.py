1f11b3e29726a750694e4491a82998cb71155d1c
mbbderobles <mbbderobles@gmail.com>
2015-09-02 16:30:41 +0800

    Updated metadata

---
+++ functions/readTable.py
@@ -0,0 +1,42 @@
+import readMeta
+
+def getRowsFromTable(tblname,columns):
+	fReader = open('./../data/'+tblname+'.txt','r')
+	tblData = {}
+	for line in fReader:
+		if(line[-1]=='\n'):
+			line = line[:-1]
+		data = line.split('|')
+		tblData[data[0]] = {}
+		i = 0
+		while i<len(columns):
+			if i==0:
+				i = i+1
+				continue
+			tblData[data[0]][columns[i]] = data[i]
+			i = i+1
+	return tblData
+
+def getColumns(tableName,tables):
+	cols = []
+	temp = tables[tableName]
+	for i in temp.keys():
+		cols.append(temp[i][0]) #sub zero since 1st element is columnName
+	return cols
+
+def printTableRows(rows,cols):
+	for k in cols:
+		print("\t%s\t" % k.upper(), end="")
+	print()
+	for k in rows.keys():
+		print("\t%s\t " % k, end="")
+		i=1
+		for j in rows[k]:
+			print("\t%s\t " % rows[k][cols[i]], end="")
+			i = i+1
+		print()
+		
+tb = readMeta.readMetaData()
+cols = getColumns("sales_h",tb)
+rows = getRowsFromTable("sales_h",cols)
+printTableRows(rows,cols)

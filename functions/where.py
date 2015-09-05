2e7ddb59ce735e77f6dd5c0eb88ba3993d9a834b
ashlynkim <ashlynbalangcod@gmail.com>
2015-09-05 23:38:29 +0800

    added function for where statement processing, still need to create separate lists for OR and AND clauses

--- functions/where.py
+++ functions/where.py
@@ -3,6 +3,21 @@ import readTable
 #using sales_h table as sample - variable readTable.data has sales_h dictionary
 #retrieved data are in list keyList
 
+#======================utils=======================================
+#taken from http://stackoverflow.com/questions/15357830/python-spliting-a-list-based-on-a-delimiter-word
+#modified 
+def regroupList(stmt, separator):
+    sublist=[]
+    for i in stmt:
+        if i == separator:            
+            yield sublist
+            sublist = []
+            continue
+        sublist.append(i)
+    yield sublist
+   
+#==================================================================
+
 #if valueToSearch is equal to 
 def dataExistInTable(sourceData, valueToSearch):
     keyList=[]
@@ -13,6 +28,21 @@ def dataExistInTable(sourceData, valueToSearch):
                
     return keyList
 
-        
+#still under construction----
+# aim is to have two separate lists for OR statements and AND statements - also need to consider precedence
+def processWhereStmt(whereStmt):   
+    andlist =  list( regroupList(whereStmt, "and"))
+    print(andlist)
+    orlist =  list( regroupList(andlist[1], "or"))
+    print(orlist)
+    print(andlist) # - need to remove the list of or from the and list
+   
 #print(readTable.data['sales_h'])
-print(dataExistsInCol(readTable.data['sales_h'], '220'))
+#print(dataExistInTable(readTable.data['sales_h'], '220'))
+#sample where clause : 
+sampleStmt = ["where", "cashier_id", "=", "1054", "and", "sales_gross_amount", "=", "220", "or", "customer_id", "=", "0" ]
+#remove "where" element
+sampleStmt.remove("where")
+processWhereStmt(sampleStmt)
+
+

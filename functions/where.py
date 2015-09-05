282ac62bf69883a3e7a6ac0ee2541bf76dbda20e
ashlynkim <ashlynbalangcod@gmail.com>
2015-09-05 18:25:51 +0800

    returns list of keys and added to a list variable

--- functions/where.py
+++ functions/where.py
@@ -1,14 +1,15 @@
-import readTable, readMeta
+import readTable
 
 #using sales_h table as sample - variable readTable.data has sales_h dictionary
-#only retrieves one key
+#retrieved data are in list keyList
 def dataExistsInCol(sourceData, valueToSearch):
+    keyList=[]
     for k in sourceData:
         for key,value in sourceData[k].items():
-           if(valueToSearch==value):                            
-               print(key +" ::")
-               return k
-    return None
+           if(valueToSearch==value):                  
+               keyList.append(k) 
+               
+    return keyList
         
-print(readTable.data['sales_h'])
+#print(readTable.data['sales_h'])
 print(dataExistsInCol(readTable.data['sales_h'], '220'))

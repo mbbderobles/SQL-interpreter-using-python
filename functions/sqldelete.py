2ddddc6c4997fc34b9590ffd333e22252a2c5dfc
ashlynkim <ashlynbalangcod@gmail.com>
2015-09-09 13:46:12 +0800

    new function to delete from source given a list of keys

--- functions/sqldelete.py
+++ functions/sqldelete.py
@@ -2,8 +2,32 @@ import where
 
 #assuming source has values
 def deleteAll(source):
+    sourceLen = len(source) + 1
     source.clear()
+    print("Successfully deleted "+ sourceLen.__str__() + " rows." )
     
+#deletes the values of the keys in keyList from source 
+#assumes the source is a hash of a single table    
+def deleteValues(source, keyList):
+    cntr =0
+    for j in keyList:
+        cntr+= deleteKey(source, j)      
+    if cntr > 0:   
+        print("Deleted " , cntr , " rows successfully.")
+    else:
+        print("No rows were deleted.")
+        
+        
+# deletes an entry from the source given a key, returns 1 if an entry is deleted and 0 otherwise   
+def deleteKey(source, key):
+    for k in source:    
+        if k == key:    
+            print("key= ", k,  ": val= "+ source[k].__str__()) 
+            del source[k]
+            return 1
+    return 0
+            
+            
 
 def findTableSource(query):
     nextInd = -1
@@ -21,8 +45,10 @@ def findTableSource(query):
     
 def processDelQuery(dataSource,query):
     if("where" in query or "WHERE" in query):
-        where.processWhereStmt() # should return keys
+        keyList=['386-337348','386-337335']
+     # keyList=  where.processWhereStmt() # should return keys in a list
+        deleteValues(dataSource[findTableSource(query)], keyList)
      # returned keys should be removed and stored back to dataSource
     else:    
         deleteAll(dataSource[findTableSource(query)])
-    
+           

4fffe94ca16ac02e5829354d24c0cfe24c76a16c
mbbderobles <mbbderobles@gmail.com>
2015-09-12 17:34:11 +0800

    SQL Update

--- functions/evalExp.py
+++ functions/evalExp.py
@@ -1,4 +1,4 @@
-import readTable,sqldelete
+import readTable,sqldelete,sqlUpdate
 
 #query = ["UPDATE","sales_h","SET","register_date","=","11/11/2015","cashier_id","=","0000"];
 query = ["Delete","*", "from", "sales_h"]; #still working on the where expression
@@ -7,23 +7,12 @@ def main():
 	if(query[0].lower() == "select"):
 		print("select function here")
 	elif(query[0].lower() == "update"):
-		update(query)
+		sqlUpdate.update(readTable.data,query)
 	else:
 		print("delete function here")
 		sqldelete.processDelQuery(readTable.data,query)
 		#to check print the dictionary for sales_h
 		
-
-def update(query):
-	i = 3											# Start with 4th element in the list
-	if("where" in query or "WHERE" in query):		# Check if query contains the where clause
-		print("to be continued...")
-	else:
-		while(i < len(query)):						# Modify all rows of the specified column/s
-			for j in readTable.data[query[1]].keys():
-				readTable.data[query[1]][j][query[i]] = query[i+2]
-			i+=3
-
 main()
 cols = readTable.getColumns("sales_h",readTable.tb)
 readTable.printTableRows(readTable.data,"sales_h",cols)
---
+++ functions/sqlUpdate.py
@@ -0,0 +1,12 @@
+import sqlWhere
+
+def update(data,query):
+	i = 3											# Start with 4th element in the list
+	if("where" in query or "WHERE" in query):		# Check if query contains the where clause
+		sqlWhere.sampleStmt.remove("where")
+		sqlWhere.processWhereStmt(sampleStmt)
+	else:
+		while(i < len(query)):						# Modify all rows of the specified column/s
+			for j in data[query[1]].keys():
+				data[query[1]][j][query[i]] = query[i+2]
+			i+=3

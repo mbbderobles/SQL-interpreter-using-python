012dda9e3168e5ff3b1a765ba5e10ea5807c8d64
mbbderobles <mbbderobles@gmail.com>
2015-09-12 17:38:49 +0800

    SQL Update

--- functions/evalExp.py
+++ functions/evalExp.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import readTable,sqldelete,sqlUpdate
-=======
-import readTable,sqldelete,sqlselect
->>>>>>> 2ddddc6c4997fc34b9590ffd333e22252a2c5dfc
+import readTable,sqldelete,sqlSelect,sqlUpdate
 
 #query = ['select','*','from','sales_h',',','sales_d'];
 
@@ -16,24 +12,6 @@ def main(query,):
 		sqldelete.processDelQuery(readTable.data,query)
 		#to check print the dictionary for sales_h
 		
-<<<<<<< HEAD
 main()
 cols = readTable.getColumns("sales_h",readTable.tb)
 readTable.printTableRows(readTable.data,"sales_h",cols)
-=======
-
-def update(query):
-	i = 3											# Start with 4th element in the list
-	if("where" in query or "WHERE" in query):		# Check if query contains the where clause
-		print("to be continued...")
-	else:
-		while(i < len(query)):						# Modify all rows of the specified column/s
-			for j in readTable.data[query[1]].keys():
-				readTable.data[query[1]][j][query[i]] = query[i+2]
-			i+=3
-
-#main(query)
-#print()
-#query2 = ['select','link_or','register_date','or_no','from','sales_h',',','sales_d']
-#main(query2)
->>>>>>> 2ddddc6c4997fc34b9590ffd333e22252a2c5dfc

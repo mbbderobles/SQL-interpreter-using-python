f533f625d9071466c470049b045b9348228644d1
mbbderobles <mbbderobles@gmail.com>
2015-09-27 00:49:57 +0800

    Added useful eval functions

--- functions/sqlUtils.py
+++ functions/sqlUtils.py
@@ -23,20 +23,67 @@ def getWhereIndex(query):
 	else:
 		return(query.index('WHERE'))
 
-# Get primary keys based on the evaluation of conditions
+# Returns true if value of the primary key already exists
+def isPrimaryValue(tbl,data,val):
+	for j in data[tbl].keys():
+		if(val == j):
+			return True
+	return False
+
+
+# Returns a list of primary keys based on the evaluation of conditions
 # squery - list containing the conditions in the where clause (WHERE [condition])
-def getKeysFromCond(data,tbl,squery):
+def getKeysFromCond(data,tb,tbl,squery):
 	pk = []												# Lists all pks that makes the condition true
 	for j in data[tbl].keys():
 		i=0
 		while(i < len(squery)):							# Evaluates all conditions after WHERE clause
-			if(data[tbl][j][squery[i]] == squery[i+2]):
-				if(i==0):
-					pk.append(j)
-				elif(i>0 and squery[i-1].lower()=="or" and j not in pk):
-					pk.append(j)
-			else:
-				if(i>0 and squery[i-1].lower()=="and" and j in pk):
-					pk.remove(j)
+			if(isPrimary(tb,tbl,squery[i])):			# Checks if column is primary key
+				if(evalCondition(j,squery[i+1],squery[i+2],getType(tb,tbl,j))):
+					if(i==0):
+						pk.append(j)
+					elif(i>0 and squery[i-1].lower()=="or" and j not in pk):
+						pk.append(j)
+				else:
+					if(i>0 and squery[i-1].lower()=="and" and j in pk):
+						pk.remove(j)
+			else:										# Columns is non-primary key
+				if(evalCondition(data[tbl][j][squery[i]],squery[i+1],squery[i+2],getType(tb,tbl,squery[i]))):
+					if(i==0):
+						pk.append(j)
+					elif(i>0 and squery[i-1].lower()=="or" and j not in pk):
+						pk.append(j)
+				else:
+					if(i>0 and squery[i-1].lower()=="and" and j in pk):
+						pk.remove(j)
 			i+=4
-	print(pk)
+	return pk
+
+# Evaluates condition, returns true or false
+def evalCondition(op1,op,op2,dType):
+	if(op=="="):
+		return (op1==op2)
+	elif(op=="<"):
+		if(dType == "int"):						# Converts values if needed
+			return (int(op1)<int(op2))
+		elif(dType == "float"):
+			return (float(op1)<float(op2))
+		else:
+			return (op1<op2)
+	elif(op==">"):
+		if(dType == "int"):						# Converts values if needed
+			return (int(op1)>int(op2))
+		elif(dType == "float"):
+			return (float(op1)>float(op2))
+		else:
+			return (op1>op2)
+	elif(op=="!="):
+		return (op1!=op2)
+
+# Returns the data type of the column
+def getType(tb,tbl,col):
+	i = 0
+	while(i<5):
+		if(col == tb[tbl][i][0]):
+			return tb[tbl][i][1]
+		i+=1

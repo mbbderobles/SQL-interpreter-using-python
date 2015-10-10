02aa98f3cf452d9fa2b68a365b3cb495acd54662
eugenebato <ea.bato@yahoo.com>
2015-10-10 13:41:03 +0800

    modified in procRecTextFile.py and inclusion of reading and writing in textfile in main.py

--- functions/procRecTextFile.py
+++ functions/procRecTextFile.py
@@ -7,13 +7,15 @@ def getRecordsFromTextFile():
 	tb = readMeta.tb
 	for i in tb.keys():
 		cols = readTable.getColumns(i.lower(),tb)
-		vRecords = readTable.getRowsFromTable(i.lower(),cols) #get the records from textfile.
-		data = readTable.addTableToHash(data,i.lower(),vRecords) #place the records to hash table.
+		#vRecords = readTable.getRowsFromTable(i.lower(),cols) #get the records from textfile. #Modified on the next line.
+		data[i]=readTable.getRowsFromTable(i.lower(),cols)
+		#data = readTable.addTableToHash(data,i.lower(),vRecords) #place the records to hash table.
 	
 	return data
 
 
 def writeRecordsToTextFile(data):
+	tb = readMeta.tb
 	for i in data.keys():
 		cols = readTable.getColumns(i.lower(),tb)
 		vColumnCount = len(cols) #get the number of columns on the table.
@@ -24,21 +26,24 @@ def writeRecordsToTextFile(data):
 			for b in data[i][a]:
 				vCurrentColumn = vCurrentColumn+1
 				fRecTextFile.write(data[i][a][b]) #print the record to the textfile.
-				if vCurrentColumn > vColumnCount: #check first if it is not the last column before printing the pipe.
+				if vCurrentColumn < vColumnCount: #check first if it is not the last column before printing the pipe.
 					fRecTextFile.write('|')
 			fRecTextFile.write('\n') #print newline on the textfile. it means that we will print a new record row.
 		fRecTextFile.close()
 
 
-#for checking purposes only. this can be deleted
 records = getRecordsFromTextFile()
+writeRecordsToTextFile(records)
 
-for k in records.keys():
-	print("\t%s\t" % k.upper(), end="")
-	print()
+##for checking purposes only. this can be deleted
+#records = getRecordsFromTextFile()
 
-	for k in records[k].keys():
-		print("\t%s\t " % k, end="")
-		print()
+#for k in records.keys():
+#	print("\t%s\t" % k.upper(), end="")
+#	print()
+
+#	for k in records[k].keys():
+#		print("\t%s\t " % k, end="")
+#		print()
 #for checking purposes only. this can be deleted
-	input('next')
+#	input('next')
